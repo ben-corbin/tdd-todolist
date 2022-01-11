ca005d2cc1fc35be910a505cafceeb6609a630e9
Ben Corbin <ben@corbin.digital>
2022-01-11 11:44:29 +0000

    All but last test passing

--- spec/TodoList.spec.js
+++ spec/TodoList.spec.js
@@ -7,16 +7,119 @@ describe("TodoList", () => {
     todoList = new TodoList()
   })
 
-  it("creates a todo item", () => {
+  // it("creates a todo item", () => {
+  //   // set up
+  //   const expected = {
+  //     id: 1,
+  //     text: "turn the heating on!",
+  //     status: "incomplete"}
+  
+  //   // execute
+  //   const result = todoList.create("turn the heating on!")
+  //   // verify
+  //   expect(result).toEqual(expected)
+  // })
+
+  // it("returns all items from the to do list", () => {
+  //   // set up
+  //   const expected = [{
+  //     id: 1,
+  //     text: "turn the heating on!",
+  //     status: "incomplete"},
+  //     {id: 2,
+  //     text: "Note two, already done", 
+  //     status: "incomplete"},
+  //   {id: 3,
+  //     text: "Note three", 
+  //     status: "incomplete"
+  //   }]
+  //   // execute
+  //   todoList.create("turn the heating on!")
+  //   todoList.create("Note two, already done")
+  //   todoList.create("Note three")
+  //   const result = todoList.returnAllObjects()
+  //   // verify
+  //   expect(result).toEqual(expected)
+  // })
+
+  // it("Set a todo completed by its ID", () => {
+  //   // set up
+  //   const expected = [{
+  //     id: 1,
+  //     text: "turn the heating on!",
+  //     status: "complete"}]
+  //   // execute
+  //   todoList.create("turn the heating on!")
+  //   const result = todoList.setStatusComplete(1)
+  //   // verify
+  //   expect(result).toEqual(expected)
+  // })
+
+  // it("Get only todo items that are incomplete", () => {
+  //   // set up
+  //   const expected = [{
+  //     id: 1,
+  //     text: "turn the heating on!",
+  //     status: "incomplete"}]
+  //   // execute
+  //   todoList.create("turn the heating on!")
+  //   todoList.create("open the windows")
+  //   todoList.setStatusComplete(2)
+  //   const result = todoList.returnIncompleteItems()
+  //   // verify
+  //   expect(result).toEqual(expected)
+  // })
+
+  // it("Get only todo items that are complete", () => {
+  //   // set up
+  //   const expected = [{
+  //     id: 1,
+  //     text: "turn the heating on!",
+  //     status: "complete"}]
+  //   // execute
+  //   todoList.create("turn the heating on!")
+  //   todoList.create("open the windows")
+  //   todoList.setStatusComplete(1)
+  //   const result = todoList.returnCompleteItems()
+  //   // verify
+  //   expect(result).toEqual(expected)
+  // })
+
+  // it("Search and return a todo item by its ID", () => {
+  //   // set up
+  //   const expected = [{
+  //     id: 1,
+  //     text: "turn the heating on!",
+  //     status: "incomplete"}]
+  //   // execute
+  //   todoList.create("turn the heating on!")
+  //   const result = todoList.search(1)
+  //   // verify
+  //   expect(result).toEqual(expected)
+  // })
+
+  // it("Search by ID & return an error message if it doesn’t exist", () => {
+  //   // set up
+  //   const expected = ("Error ID does not exist")
+  //   // execute
+  //   todoList.create("turn the heating on!")
+  //   const result = todoList.search(2)
+  //   // verify
+  //   expect(result).toEqual(expected)
+  // })
+
+  it("Remove and return a todo item by its ID", () => {
     // set up
-    const expected = {
+    const expected = [{
       id: 1,
       text: "turn the heating on!",
-      status: "incomplete"
-    }
+      status: "incomplete"}]
     // execute
-    const result = todoList.create("turn the heating on!")
+    todoList.create("turn the heating on!")
+    const result = todoList.removeItem(1)
     // verify
     expect(result).toEqual(expected)
   })
+
+
 })
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,85 @@
+// const toDoObject = {
+//     id: [],
+//     text: [],
+//     status: [],
+// };
+//Create a todo item that has an ID, text description, and starts off incomplete DONE
+// Get all todo items 
+// Set a todo completed by its ID
+// Get only todo items that are incomplete
+// Get only todo items that are complete
+// Search and return a todo item by its ID, or return a message saying it doesn’t exist
+// Remove a todo item by its ID
+
+
+
 class TodoList {
+    constructor() {
+        this.toDoArray = [];
+        this.toDoCounter = 1
+    }
+   create(note) {
+         const todo = {
+                id: this.toDoCounter,
+                text: note,
+                status: 'incomplete'
+            }
+            this.toDoCounter++;
+            this.toDoArray.push(todo);
+            return todo
+        }
+    
+    returnAllObjects() {
+      return this.toDoArray
+}
+    setStatusComplete(id) {
+        this.toDoArray[id-1].status = 'complete'
+        console.log(this.toDoArray[id-1])
+        return this.toDoArray
+    }
+
+    returnIncompleteItems() {
+        const incompleteItems = []
+        for (let i = 0; i < this.toDoArray.length; i++) {
+            if (this.toDoArray[i].status === 'incomplete') {
+                incompleteItems.push(this.toDoArray[i])
+            }
+        }
+        return incompleteItems
+    }
+
+    returnCompleteItems() {
+        const completeItems = []
+        for (let i = 0; i < this.toDoArray.length; i++) {
+            if (this.toDoArray[i].status === 'complete') {
+                completeItems.push(this.toDoArray[i])
+            }
+        }
+        return completeItems
+    }
+
+    search(id) {
+        const searchItems = []
+        for (let i = 0; i < this.toDoArray.length; i++) {
+            if (this.toDoArray[i].id === (id)) {
+                searchItems.push(this.toDoArray[i])
+            }
+            else return "Error ID does not exist"
+        }
+        return searchItems
+    }
+
+    removeItem(id) {
+        const removedItems = []
+        for (let i = 0; i < this.toDoArray.length; i++) {
+            if (this.toDoArray[i].id === (id)) {
+                removedItems.splice(this.toDoArray[i], 1)
+            }
+        }
+        return removedItems
+    }
 
 }
+
 
 module.exports = TodoList
